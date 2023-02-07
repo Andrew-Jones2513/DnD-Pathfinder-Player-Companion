8948491a346268c3d93673c44fdfd0aa735b092e
Andrew Jones <Andrew_Jones@insideranken.org>
2023-02-07 09:57:16 -0600

    Solution Build
    Start of solution with a save button

---
+++ PlayerCompanion/PlayerCompanion/Companion.cs
@@ -0,0 +1,15 @@
+namespace PlayerCompanion
+{
+    public partial class Companion : Form
+    {
+        public Companion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //Save button
+        }
+    }
+}
